08e5e280a09200e9a8bb29f06694f8c00e1243ff
Stanislas Morbieu <stanislas.morbieu@gmail.com>
2020-09-28 10:16:52 +0200

    Do not check NumPy and SciPy requirements during the installation.

--- setup.py
+++ setup.py
@@ -1,22 +1,9 @@
-from __future__ import print_function
-import sys
 from setuptools import setup, find_packages
+
 
 with open('requirements.txt') as f:
     INSTALL_REQUIRES = [l.strip() for l in f.readlines() if l]
 
-
-try:
-    import numpy  # NOQA
-except ImportError:
-    print('numpy is required during installation')
-    sys.exit(1)
-
-try:
-    import scipy  # NOQA
-except ImportError:
-    print('scipy is required during installation')
-    sys.exit(1)
 
 setup(
     name='spherecluster',
